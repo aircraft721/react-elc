070c1f0c3e158e65a7b93502602f8af79bc92b80
Dan Constantinescu <dan_constantinescu@outlook.com>
2018-06-27 00:18:10 +0300

    initial commit with some basic setup

---
+++ webpack.common.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: ['babel-polyfill', './src/index.ts'],
+    module: {
+        rules: [
+
+        ]
+    },
+    plugins: [
+
+    ]
+}
